e5201f4e64e7b2c07da958a6e0575ee6bbfe5b0a
Datpronguyen <nguyendataht@gmail.com>
2019-12-13 15:35:19 +0700

    build

---
+++ precache-manifest.b02fe6fdb6f83b5c01a5188eff205bb1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7c5d71802ad1335f3863b56adafacab9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3ea0bb98c10203fd87fb",
+    "url": "/static/css/main.0740b91d.chunk.css"
+  },
+  {
+    "revision": "a4eecb84d5e07f52ba62",
+    "url": "/static/js/2.a854487f.chunk.js"
+  },
+  {
+    "revision": "d58d4753ce62878395a8e4503036e8d6",
+    "url": "/static/js/2.a854487f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3ea0bb98c10203fd87fb",
+    "url": "/static/js/main.e35f9638.chunk.js"
+  },
+  {
+    "revision": "8829d48e6f624375c885",
+    "url": "/static/js/runtime-main.a752e2d4.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "226c379857de75f595af7cb679f126b6",
+    "url": "/static/media/mrdat.226c3798.jpg"
+  }
+]);
